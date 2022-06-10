c5bf1b4ceb535f835c233e5d99e3f407fab2a353
aaronhepting <aaronhepting@me.com>
2022-06-10 00:51:18 -0600

    Updated Navigation
    Cleaned up navigation.

--- App.js
+++ App.js
@@ -1,53 +1,39 @@
-import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
 import {
-  ViroARScene,
-  ViroText,
   ViroTrackingStateConstants,
-  ViroARSceneNavigator,
   ViroVRSceneNavigator,
+  ViroARSceneNavigator,
   ViroSceneNavigator,
 } from '@viro-community/react-viro';
 
-import MyScene from './Viro360Theatre';
 import FirstScene from './FirstScene';
 
-const HelloWorldSceneAR = () => {
-  const [text, setText] = useState('Initializing AR...');
-
-  function onInitialized(state, reason) {
-    console.log('guncelleme', state, reason);
-    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
-      setText('Hello World!');
-    } else if (state === ViroTrackingStateConstants.TRACKING_NONE) {
-      // Handle loss of tracking
-    }
-  }
-
-  return (
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
-    </ViroARScene>
-  );
-};
-
 var vrScenes = {
-    'FirstScene': require('./FirstScene'),
+  'FirstScene': require('./FirstScene'),
 }
 
+// VR SECTION
 export default () => {
   return (
-      <ViroVRSceneNavigator
-            initialScene={{
-              scene: vrScenes['FirstScene'],
-            }}/>
+    <ViroVRSceneNavigator
+      initialScene={{
+        scene: require('./FirstScene'),
+      }}
+    />
   );
 };
+
+// AR SECTION
+// export default () => {
+//   return (
+//     <ViroARSceneNavigator
+//       initialScene={{
+//         scene: require('./FirstScene'),
+//       }}
+//     />
+//   );
+// };
 
 var styles = StyleSheet.create({
   f1: {flex: 1},
--- FirstScene.js
+++ FirstScene.js
@@ -1,23 +1,7 @@
-
-
-
-/**
- * Copyright (c) 2015-present, Viro Media, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
 'use strict';
 
-/**
- * Pull in all imports required for the controls within this scene.
- */
 import React, { Component } from 'react';
 import {StyleSheet} from 'react-native';
-
 import {
   ViroScene,
   ViroARScene,
@@ -29,75 +13,43 @@ import {
   ViroText
 } from '@viro-community/react-viro';
 
-/**
- * Set all the images and assets required in this scene.
- */
-var westLakeTowersScene = require('./FirstScene');
-var weworkImage = require('./res/wework_title.png');
-
 export default class MainScene extends Component {
   constructor() {
     super();
-
-    // set initial state
-    this.state = {
-      runShowTitleAnimation : false,
-    }
-
-    // bind `this` to functions
-    this._onBackgroundPhotoLoadEnd = this._onBackgroundPhotoLoadEnd.bind(this);
-    this._onTitleClicked = this._onTitleClicked.bind(this);
   }
 
-  /**
-   * Renders a scene with a 360 Photo background that contains a single WeWork ViroImage. This
-   * image will be faded/scaled in with the "showTitleAnimation" when the scene first appears. Clicking on
-   * the WeWork ViroImage will launch the user into the next scene (WestLakeTowers).
-   */
+  // VR SECTION
   render() {
     return (
       <ViroScene style={styles.container}>
-          <Viro360Image source={require('./res/dark_theatre.jpg')} />
-
-          <ViroVideo
-          source={require("./res/shoot-1.mp4")}
+        <ViroVideo
+          source={require("./res/vr-1.mp4")}
           volume={1.0}
           loop={true}
-          position={[0, 3.9, -45]}
-          scale={[44, 22, 1]}
           stereoMode={'leftRight'}
-          />
-
-
-
+        />
       </ViroScene>
     );
   }
 
-  /**
-   * Callback function for when image has finished loading in the Viro360Photo.
-   * We then animate the WeWork ViroImage into the scene through the
-   * setting of state runShowTitleAnimation.
-   */
-  _onBackgroundPhotoLoadEnd() {
-      this.setState({
-          runShowTitleAnimation:true
-      });
-  }
-
-  /**
-   * Callback function for when the user taps on the WeWork ViroImage
-   * where we navigate into the second scene.
-   */
-  _onTitleClicked() {
-      this.props.sceneNavigator.push({scene:westLakeTowersScene});
-  }
+  // AR SECTION
+  // render() {
+  //   return (
+  //     <ViroARScene style={styles.container}>
+  //       <Viro360Image source={require('./res/dark_theatre.jpg')} />
+  //
+  //       <ViroVideo
+  //         source={require("./res/mining.mp4")}
+  //         volume={1.0}
+  //         loop={true}
+  //         position={[0, 3.9, -45]}
+  //         scale={[44, 22, 1]}
+  //       />
+  //     </ViroARScene>
+  //   );
+  // }
 }
 
-/**
- * Declare all custom flex box styles here to be reference by the
- * controls above.
- */
 var styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -111,7 +63,7 @@ var styles = StyleSheet.create({
  * Declare all your animations here. They'll be referenced by the animation props.
  */
 ViroAnimations.registerAnimations({
-    showTitleAnimation: {properties:{scaleX:2, scaleY:2, scaleZ:2, opacity:1.0}, easing:"PowerDecel", duration:1000},
+    showTitleAnimation: { properties:{scaleX:2, scaleY:2, scaleZ:2, opacity:1.0 }, easing: "PowerDecel", duration: 1000 },
 });
 
 module.exports = MainScene;
